645562a6c60d91406eb77b09cb8e70d5b45bb462
0xshiku <olachico@icloud.com>
2025-01-20 18:05:52 +0000

    feat: verify if user is logged in

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,11 @@
 from jose import JWTError, jwt
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
+from . import schemas
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 # To get a random key like this run: openssl rand -hex 32
 SECRET_KEY = "b6b801fba5e3deb719a36216c211f5aa187e46a0019b742bf7b18c97bf0a1092"
@@ -10,9 +16,32 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_enconde = data.copy()
 
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_enconde.update({"exp": expire})
 
     enconded_jwt = jwt.encode(to_enconde, SECRET_KEY, algorithm=ALGORITHM)
 
     return enconded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+
+        token_data = schemas.TokendData(id=id)
+    except JWTError:
+        raise credentials_exception
+
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_access_token(token, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -12,11 +12,11 @@ def login(user_credentials=schemas.UserLogin, db: Session = Depends(database.get
     if not user:
 
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     # Create a Token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
