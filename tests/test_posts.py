dc6038048240dcee0c090b44c38a5edf3122861f
0xshiku <olachico@icloud.com>
2025-01-26 16:04:46 +0000

    test: unauthorized post request

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -12,3 +12,8 @@ def test_get_all_posts(authorized_client, test_posts):
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
     assert posts_list[0].Post.id == test_posts[0].id
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
