6c9b56358ba6d6080e2bc2643dae79a79fa77fe1
0xshiku <olachico@icloud.com>
2025-01-26 15:36:28 +0000

    test: test token validation

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,8 @@
 import pytest
+from jose import jwt
 from app import schemas
+from app.config import settings
+
 from .database import client, session
 
 
@@ -28,5 +31,11 @@ def test_login_user(client, test_user):
     res = client.post(
         "/login", data={"username": test_user["email"], "password": test_user["password"]}
     )
-    print(res.json())
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
