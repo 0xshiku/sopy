1b115314c21a4d85ce7aba2327773732adf88493
0xshiku <olachico@icloud.com>
2025-01-26 15:28:43 +0000

    test: adds test fixture and database setup

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,14 +1,32 @@
-from fastapi.testclient import TestClient
-from app.main import app
+import pytest
 from app import schemas
+from .database import client, session
 
-client = TestClient(app)
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "chico@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post(
         "/users/", json={"email": "hello123@gmail.com", "password": "password123"})
 
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
     assert res.status_code == 201
+
+
+def test_login_user(client, test_user):
+    res = client.post(
+        "/login", data={"username": test_user["email"], "password": test_user["password"]}
+    )
+    print(res.json())
+    assert res.status_code == 200
